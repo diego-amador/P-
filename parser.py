fc2a0c46fc67b692164a5be48e8133b938e3c3b4
Javier Velez <javier.velez12@upr.edu>
2018-11-13 23:21:39 -0400

    parser now extracts important values as their initial type

--- parser.py
+++ parser.py
@@ -12,58 +12,58 @@ def p_expression_ID(p):
     """expresion : ID ASSIGN Operation COLON Function LPAREN ParameterList RPAREN Location END
                  | APPEND ID TO ID END
                  | ROTATE ID AROUND ID END"""
-    if p[1] == 'append'     : p[0] = p[0]
-    elif p[1] == 'rotate'   : p[0] = p[0]
-    else                    : p[0] = p[0]
-    print (*p)
+    # if p[1] == 'append'     : p[0] = p[0]
+    # elif p[1] == 'rotate'   : p[0] = p[0]
+    # else                    : p[0] = p[0]
+    p[0] = p[0]
+    print (*p)  #OPTIONAL FOR NOW
 
 
 def p_param_list(p):
     """ParameterList : Parameter AND ParameterList 
                      | Parameter 
                      | empty"""
-    if len(p) == 1: p[0] = ''
-    elif len(p) < 3: p[0] = p[1]
-    else: p[0]  = p[1] + ' ' + p[2] + ' ' + p[3]
+    if len(p) < 3: p[0] = p[1]      #This covers "Paramater And ParameterList" and "Parameter"
+    else: p[0]  = (p[1], p[3])      #This covers "empty"
 
 def p_parameter(p):
     """Parameter : ID ASSIGN DIGIT 
                  | ID ASSIGN String
                  | LINE ASSIGN String """
-    p[0] = p[1] + ' ' + p[2] + ' ' + str(p[3])
+    p[0] = p[3]     #The value of interest is in index 3
 
 def p_string(p) :
     'String : QUOTE ID QUOTE'
-    p[0] = p[2]
+    p[0] = p[2]     #This is used to determine the color
 
 def p_operation(p):
     'Operation : DRAW'
-    p[0] = "draw"   #No other function other than draw as of now
+    p[0] = p[1]     #No other function other than draw as of now
 
 def p_function(p):
     """Function : SIN 
                 | CIRCLE 
                 | GRID 
                 | LINE"""
-    if p[1] == 'sin' : p[0] = 'sin'
-    elif p[1] == 'circle' : p[0] = 'circle'
-    elif p[1] == 'grid' : p[0] = 'grid'
-    elif p[1] == 'line' : p[0] = 'line'
-
+    p[0] = p[1]     
+ 
 def p_location(p):
     """Location : REGARDING Coordinate
                 | empty"""
-    if p[1] == '@': p[0] = p[1] + ' ' + p[2]
-    else: p[0] = 'DEFAULT'  #This is the indicator to choose default paramters
+    if p[1] == '@': p[0] = p[2]     #This covers non DEFAULT coordinates
+    else: p[0] = p[1]
 
 def p_coordinate(p):
     """Coordinate : ID 
                   | LPAREN DIGIT COMMA DIGIT RPAREN"""
-    p[0] = p[1]
+    if p[1] != '(': p[0] = p[1]     #This covers coordinates represented as ID
+    else: p[0] = (p[2], p[4])       #Coordinate as (x, y)
+
 
 def p_empty(p):
     'empty :'
-    pass
+    p[0] = 'DEFAULT'    #This is the indicator to choose default paramters
+
 
 # Error rule for syntax errors
 def p_error(p):
@@ -72,7 +72,7 @@ def p_error(p):
 # Build the parser
 parser = yacc.yacc()
 
-s = '''A = draw : sin (color = "blue" and amplitud = 5); 
+s = '''A = draw : circle (radius = 5 and color = "blue") @ (1, 5); 
 '''
 
 result = parser.parse(s)
